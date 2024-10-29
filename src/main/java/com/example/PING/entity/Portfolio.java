f6bf2828814df6f5718c73d267682ef104209011
SongeunKim <ucss22002@gmail.com>
2024-10-29 16:23:29 +0900

    [FEAT] Portfolio

--- src/main/java/com/example/PING/entity/Portfolio.java
+++ src/main/java/com/example/PING/entity/Portfolio.java
@@ -2,6 +2,8 @@ package com.example.PING.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -40,9 +42,11 @@ public class Portfolio {
 
     private String description;
 
+    @CreationTimestamp
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
 
+    @UpdateTimestamp
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 }
