a47c4748be1194a532154e2153de96773f058eca
SongeunKim <ucss22002@gmail.com>
2024-10-31 20:04:55 +0900

    [FEAT] PortfolioController

--- src/main/java/com/example/PING/service/PortfolioService.java
+++ src/main/java/com/example/PING/service/PortfolioService.java
@@ -3,7 +3,11 @@ package com.example.PING.service;
 import com.example.PING.dto.PortfolioRequestDto;
 import com.example.PING.dto.PortfolioResponseDto;
 import com.example.PING.entity.Portfolio;
+import com.example.PING.entity.Template;
+import com.example.PING.entity.User;
 import com.example.PING.repository.PortfolioRepository;
+import com.example.PING.repository.TemplateRepository;
+import com.example.PING.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +19,8 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class PortfolioService {
     private final PortfolioRepository portfolioRepository;
+    private final UserRepository userRepository;
+    private final TemplateRepository templateRepository;
 
     @Transactional
     public PortfolioResponseDto createPortfolio(PortfolioRequestDto portfolioRequestDto) {
@@ -22,8 +28,20 @@ public class PortfolioService {
         portfolio.setTitle(portfolioRequestDto.getTitle());
         portfolio.setDescription(portfolioRequestDto.getDescription());
         // 사용자 및 템플릿 설정은 추가적인 로직 필요
+//        User user = userRepository.findById(portfolioRequestDto.getUserId())
+//                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+//        portfolio.setUser(user);
+        User user = userRepository.findById(portfolioRequestDto.getUserId())
+                .orElseThrow(() -> new IllegalArgumentException("User not found with ID: " + portfolioRequestDto.getUserId()));
+        portfolio.setUser(user);
+
+        Template template = templateRepository.findById(portfolioRequestDto.getTemplateId())
+                .orElseThrow(() -> new IllegalArgumentException("Template not found"));
+        portfolio.setTemplate(template);
+
         return convertToResponseDto(portfolioRepository.save(portfolio));
     }
+
 
     @Transactional(readOnly = true)
     public List<PortfolioResponseDto> getAllPortfolios() {
