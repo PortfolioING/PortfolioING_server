2ace8c9f3bd3563eaa331941d535374f5aedb51d
seanlancekim0 <122666778+seanlancekim0@users.noreply.github.com>
2024-11-29 12:19:22 +0900

    [UPDATE] API에 getProjectById() 추가

--- src/main/java/com/example/PING/controller/ProjectController.java
+++ src/main/java/com/example/PING/controller/ProjectController.java
@@ -1,16 +1,14 @@
 package com.example.PING.controller;
 
 import com.example.PING.dto.request.ProjectRequestDto;
+import com.example.PING.dto.response.PortfolioResponseDto;
 import com.example.PING.dto.response.ProjectIdResponseDto;
 import com.example.PING.dto.response.ProjectResponseDto;
 import com.example.PING.service.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/projects")
@@ -23,6 +21,12 @@ public class ProjectController {
     public ResponseEntity<ProjectIdResponseDto> createProject(@RequestBody ProjectRequestDto projectRequestDto) {
         ProjectIdResponseDto response = projectService.createProject(projectRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/{project_id}")
+    public ResponseEntity<ProjectResponseDto> getProjectById(@PathVariable("project_id") Long projectId) {
+        ProjectResponseDto response = projectService.getProjectById(projectId);
+        return ResponseEntity.ok(response);
     }
 
 }
