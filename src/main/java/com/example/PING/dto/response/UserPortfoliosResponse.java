6c4dedbd937b920c7b9a9f801de7b6afea54868c
simchaehyeon <tlacogussla@konkuk.ac.kr>
2024-12-29 23:25:45 +0900

    [REFACTOR] userPortfoliosResponseDto record 변환

--- src/main/java/com/example/PING/dto/response/UserPortfoliosResponse.java
+++ src/main/java/com/example/PING/dto/response/UserPortfoliosResponse.java
@@ -1,13 +1,9 @@
 package com.example.PING.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
 import java.util.List;
 
-@Data
-@AllArgsConstructor
-public class UserPortfoliosResponse {
-    private Long userId;
-    private List<PortfolioResponseDto> portfolios;
+public record UserPortfoliosResponse (
+        Long userId,
+        List<PortfolioResponseDto> portfolios
+){
 }
