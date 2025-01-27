55911f0a4c1f1a97300a2bd8720ed0ddfd080236
seanlancekim0 <122666778+seanlancekim0@users.noreply.github.com>
2025-01-27 21:04:03 +0900

    [UPDATE] PortfolioCreateResponse dto 수정

--- src/main/java/com/example/PING/portfolio/dto/response/PortfolioCreateResponse.java
+++ src/main/java/com/example/PING/portfolio/dto/response/PortfolioCreateResponse.java
@@ -3,7 +3,6 @@ package com.example.PING.portfolio.dto.response;
 import java.time.LocalDateTime;
 
 public record PortfolioCreateResponse(
-        Long portfolioId,
-        LocalDateTime createdAt
+        Long portfolioId
 ){
 }
