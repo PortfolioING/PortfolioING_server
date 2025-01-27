d758c871608071710f0113795a73ad0e8d065eba
seanlancekim0 <122666778+seanlancekim0@users.noreply.github.com>
2025-01-27 22:03:52 +0900

    [UPDATE] PortfolioService updatePortfolio 수정

--- src/main/java/com/example/PING/portfolio/service/PortfolioService.java
+++ src/main/java/com/example/PING/portfolio/service/PortfolioService.java
@@ -4,24 +4,18 @@ import com.example.PING.component.entity.Component;
 import com.example.PING.component.repository.ComponentRepository;
 import com.example.PING.domain.repository.DomainRepository;
 import com.example.PING.portfolio.dto.request.PortfolioCreateRequest;
-import com.example.PING.portfolio.dto.request.PortfolioRequest;
+import com.example.PING.portfolio.dto.request.PortfolioUpdateRequest;
 import com.example.PING.portfolio.dto.request.PortfolioUpdateTemplateRequest;
-import com.example.PING.portfolio.dto.response.PortfolioCreateResponse;
-import com.example.PING.portfolio.dto.response.PortfolioUpdateTemplateResponse;
-import com.example.PING.portfolio.dto.response.PortfolioResponse;
+import com.example.PING.portfolio.dto.response.*;
 import com.example.PING.portfolio.entity.Portfolio;
 import com.example.PING.portfolio.repository.PortfolioRepository;
 import com.example.PING.template.entity.Template;
 import com.example.PING.template.repository.TemplateRepository;
 import com.example.PING.user.entity.User;
-import com.example.PING.portfolio.dto.response.PortfolioDemoResponse;
 import com.example.PING.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -88,47 +82,31 @@ public class PortfolioService {
     }
 
     @Transactional
-    public PortfolioResponse updatePortfolio(Long portfolioId, PortfolioRequest portfolioRequest) {
+    public PortfolioUpdateResponse updatePortfolio(Long portfolioId, PortfolioUpdateRequest portfolioUpdateRequest) {
         Portfolio portfolio = portfolioRepository.findById(portfolioId)
                 .orElseThrow(() -> new IllegalArgumentException("Portfolio not found with ID: " + portfolioId));
-
-        // 포트폴리오 내용 필드 업데이트
-//        if (portfolioRequest.title() != null) {
-//            portfolio.setTitle(portfolioRequest.title());
-//        }
-//        if (portfolioRequest.description() != null) {
-//            portfolio.setDescription(portfolioRequest.description());
-//        }
-//        if (portfolioRequestDto.mainColor() != null) {
-//            portfolio.setMainColor(portfolioRequestDto.mainColor());
-//        }
-//        if (portfolioRequestDto.subColor() != null) {
-//            portfolio.setSubColor(portfolioRequestDto.subColor());
-//        }
-//        if (portfolioRequestDto.backgroundColor() != null) {
-//            portfolio.setBackgroundColor(portfolioRequestDto.backgroundColor());
-//        }
-//        수정 필요!!!!!!!!
-
-        return PortfolioResponse.from(portfolioRepository.save(portfolio));
-    }
-
-    @Transactional
-    public PortfolioUpdateTemplateResponse updateTemplate(Long portfolioId, PortfolioUpdateTemplateRequest requestDto) {
-        Portfolio portfolio = portfolioRepository.findById(portfolioId)
-                .orElseThrow(() -> new IllegalArgumentException("Portfolio not found with ID: " + portfolioId));
-
-        Template template = templateRepository.findById(requestDto.templateId())
-                .orElseThrow(() -> new IllegalArgumentException("Template not found with ID: " + requestDto.templateId()));
 
         // @Transactional의 DirtyChecking으로 save 없이 수정 사항 DB에 반영
-        portfolio.updatePortfolioTemplate(template);
-        return new PortfolioUpdateTemplateResponse(
-                portfolio.getPortfolioId(),
-                portfolio.getTemplate().getTemplateId(),
-                portfolio.getUpdatedAt()
-        );
+        portfolio.updatePortfolioTitleImg(portfolioUpdateRequest.titleImg());
+        return PortfolioUpdateResponse.from(portfolio);
     }
+
+//    @Transactional
+//    public PortfolioUpdateTemplateResponse updateTemplate(Long portfolioId, PortfolioUpdateTemplateRequest requestDto) {
+//        Portfolio portfolio = portfolioRepository.findById(portfolioId)
+//                .orElseThrow(() -> new IllegalArgumentException("Portfolio not found with ID: " + portfolioId));
+//
+//        Template template = templateRepository.findById(requestDto.templateId())
+//                .orElseThrow(() -> new IllegalArgumentException("Template not found with ID: " + requestDto.templateId()));
+//
+//        // @Transactional의 DirtyChecking으로 save 없이 수정 사항 DB에 반영
+//        portfolio.updatePortfolioTemplate(template);
+//        return new PortfolioUpdateTemplateResponse(
+//                portfolio.getPortfolioId(),
+//                portfolio.getTemplate().getTemplateId(),
+//                portfolio.getUpdatedAt()
+//        );
+//    }
 
     @Transactional
     public void deletePortfolio(Long portfolioId) {
