4aa440efdc8b3b341ec8aed251defc3008ff050b
seanlancekim0 <122666778+seanlancekim0@users.noreply.github.com>
2025-03-12 15:59:18 +0900

    [UPDATE] PortfolioRepository 최신순, 좋아요순 정렬 메소드 추가

--- src/main/java/com/example/PING/portfolio/repository/PortfolioRepository.java
+++ src/main/java/com/example/PING/portfolio/repository/PortfolioRepository.java
@@ -2,6 +2,8 @@ package com.example.PING.portfolio.repository;
 
 import com.example.PING.domain.entity.Domain;
 import com.example.PING.portfolio.entity.Portfolio;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,6 @@ public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
     Portfolio findByDomain(Domain domain);
     // 추가적인 쿼리 메서드를 정의할 수 있습니다.
 //    List<Portfolio> findByUserId(Long userId);
+    Page<Portfolio> findAllByOrderByCreatedAtDesc(Pageable pageable); // 최신순 정렬
+    Page<Portfolio> findAllByOrderByLikesDesc(Pageable pageable);    // 좋아요순 정렬
 }
