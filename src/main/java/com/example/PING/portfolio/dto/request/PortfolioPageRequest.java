4ddb496f6390641ad933b5e20e59e401ba66218f
seanlancekim0 <122666778+seanlancekim0@users.noreply.github.com>
2025-03-12 16:10:36 +0900

    [UPDATE] PortfolioPageRequest 설명 주석 추가

--- src/main/java/com/example/PING/portfolio/dto/request/PortfolioPageRequest.java
+++ src/main/java/com/example/PING/portfolio/dto/request/PortfolioPageRequest.java
@@ -8,7 +8,7 @@ import lombok.AllArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PortfolioPageRequest {
-    private int page = 0;
-    private int size = 50;      // 한 페이지에 포함되는 최대 Portfolio 개수
-    private String sort;        // "latest" or "likes"
+    private int page = 0;   // 현재 page 번호
+    private int size = 50;  // 한 페이지에 포함되는 최대 Portfolio 개수
+    private String sort;    // "latest" or "likes"
 }
