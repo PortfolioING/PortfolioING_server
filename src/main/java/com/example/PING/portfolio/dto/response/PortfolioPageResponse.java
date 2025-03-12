15cb209c206f6ec001ae8745489398b3cb92c6cd
seanlancekim0 <122666778+seanlancekim0@users.noreply.github.com>
2025-03-12 15:58:49 +0900

    [UPDATE] PortfolioPageResponse 생성 메소드 추가

--- src/main/java/com/example/PING/portfolio/dto/response/PortfolioPageResponse.java
+++ src/main/java/com/example/PING/portfolio/dto/response/PortfolioPageResponse.java
@@ -1,7 +1,9 @@
 package com.example.PING.portfolio.dto.response;
 
+import com.example.PING.portfolio.entity.Portfolio;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -12,4 +14,13 @@ public class PortfolioPageResponse {
     private int totalPages; // 총 페이지 수
     private long totalElements; // 페이지 내 Portfolio 수
     private int currentPage;    // 현재 페이지 번호
+
+    public static PortfolioPageResponse from(List<PortfolioResponse> portfolioResponses, Page<Portfolio> portfolioPage) {
+        return new PortfolioPageResponse(
+                portfolioResponses,
+                portfolioPage.getTotalPages(),
+                portfolioPage.getTotalElements(),
+                portfolioPage.getNumber()
+        );
+    }
 }
