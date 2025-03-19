5d5afdb0c635614fa4ad68150e15a9d336ef56d5
seanlancekim0 <122666778+seanlancekim0@users.noreply.github.com>
2025-03-19 12:04:12 +0900

    [ADD] GPT 요청 String 생성

--- src/main/java/com/example/PING/OpenAI/service/GPTService.java
+++ src/main/java/com/example/PING/OpenAI/service/GPTService.java
@@ -25,11 +25,16 @@ public class GPTService {
     private final ComponentRepository componentRepository;
 
     public ChatGPTResponse chat(Long componentId, String prompt) {
-        // GPT에 요청하는 prompt에 들어갈 내용의 확장을 위해 Component 객체를 사용한다.
+        // GPT에 요청하는 reqStr에 포함되는 내용의 확장을 위해 Component 객체를 사용한다.
         Component component = componentRepository.findById(componentId)
                 .orElseThrow(() -> new RuntimeException("Component not found with id " + componentId));
 
-        String reqStr = component.getTag() + prompt;
+        // GPT request String 생성
+        String reqStr = new StringBuilder()
+                .append("Component 유형: ").append(component.getTag()).append(", ")
+                .append("Component 내용: ").append(component.getText()).append(", ")
+                .append("Component 대한 요구사항 prompt: ").append(prompt).toString();
+
         ChatGPTRequest request = new ChatGPTRequest(model, reqStr);
         return template.postForObject(apiURL, request, ChatGPTResponse.class);
     }
